6c18b318bdae78ff39006c03778d5f4682e5a9c3
Evie Porter <evie@paceccs.com>
2024-04-26 06:27:27 +0100

    Add page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import EntryPage from "./pages/entry/EntryPage";
-import HistoryPage from "./pages/history/HistoryPage";
+import { lazy } from "react";
 import ThoughtStorageProvider from "./providers/ThoughtStorageProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+const HistoryPage = lazy(() => import("./pages/history/HistoryPage"))
+const EntryPage = lazy(() => import("./pages/entry/EntryPage"))
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <EntryPage />
+  },
+  {
+    path: "/history",
+    element: <HistoryPage />
+  },
+]);
 
 function App() {
   return (
     <>
       <ThoughtStorageProvider>
-        <HistoryPage />
-        <EntryPage />
+        <RouterProvider router={router} />
       </ThoughtStorageProvider>
     </>
   );
